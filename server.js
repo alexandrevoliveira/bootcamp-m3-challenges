b2b8d2f352414e882d7640fea655387e2733fcef
alexandre <alexandrevoliveira30@gmail.com>
2020-04-21 22:19:48 -0300

    modified server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-serve
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/", function(req, res){
+    return res.render("about")
+})
+
+server.get("/curses", function(req, res){
+    return res.render("curses")
+})
+
+server.use(function(req, res){
+    res.status(404).render("not-found")
+})
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
