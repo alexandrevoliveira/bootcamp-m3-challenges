5dc88d1a6ea5aeca514f71cfed5227532f1fda70
alexandre <alexandrevoliveira30@gmail.com>
2020-04-22 23:48:56 -0300

    modified data.js

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 module.exports = [
     {
         id: "starter",
-        number: 01,
+        number: 1,
         logo: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
         description: '<span id="1">Torne-se um programador desejado</span> no mercado com esses cursos gratuitos',
         price: "Gratuito"
     },
     {
         id: "launchbase",
-        number: 02,
+        number: 2,
         logo: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
         description: '<span id="2">Domine programacao do zero</span> e tenha acesso às melhores oportunidades do mercado',
         price: "R$ 960,00"
     },
     {
         id: "gostack",
-        number: 03,
+        number: 3,
         logo: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
         description: '<span id="3">Treinamento imersivo</span> nas tecnologias mais modernas de desenvolvimento web e mobile',
         price: "R$ 1600,00"
